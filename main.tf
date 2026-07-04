46a9b278add98b0e20b84653e78020fe0e588c13
Aditya M Khiroji <adityakhiroji2@gmail.com>
2026-07-04 17:47:04 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,51 @@
+resource "aws_s3_bucket" "state_bucket" {
+
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+    Owner       = "Aditya"
+  }
+
+}
+
+resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
+
+  bucket = aws_s3_bucket.state_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_bucket_encryption" {
+
+  bucket = aws_s3_bucket.state_bucket.id
+
+  rule {
+
+    apply_server_side_encryption_by_default {
+
+      sse_algorithm = "AES256"
+
+    }
+
+  }
+
+}
+
+resource "aws_s3_bucket_public_access_block" "state_bucket_public_access" {
+
+  bucket = aws_s3_bucket.state_bucket.id
+
+  block_public_acls       = true
+
+  block_public_policy     = true
+
+  ignore_public_acls      = true
+
+  restrict_public_buckets = true
+
+}
